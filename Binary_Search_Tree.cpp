9cfcafff63f079b6bc256ba330e70b02cfdb9fd9
Udit Jethva <code.uditjethva@gmail.com>
2023-09-04 13:58:38 +0530

    completed delete_node function and added traversal methods in bst

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -23,17 +23,12 @@ class bs_node{
 
 class BST{
 
-    private:
-        
-      
-
     public:
 
         bs_node * root;
-        int nodes;
+        int nodes; 
 
         BST(){
-            cout << "Created new BST" << endl;
             nodes = 0;
             root = nullptr;
         }
@@ -149,6 +144,7 @@ class BST{
                         nodes--;
                         delete root;
                     }else{
+
                         if((the_node->parent)->data > the_node->data){
                             (the_node->parent)->left = nullptr;
                             delete the_node;
@@ -159,9 +155,80 @@ class BST{
                             nodes--;
                         }
                     }
-                }else if(the_node->right != nullptr && the_node->left != nullptr){
-                    //...
+                }else if(the_node->right != nullptr && the_node->left == nullptr){
+                    if((the_node->parent)->right == the_node){
+
+                        ((the_node)->parent)->right = the_node->right;
+                        (the_node->right)->parent = the_node->parent;
+                        delete the_node;
+                        nodes--;
+
+                    }else{
+
+                        ((the_node)->parent)->left = the_node->right;
+                        (the_node->right)->parent = the_node->parent;
+                        delete the_node;
+                        nodes--; 
+
+                    }
+                }else if(the_node->left != nullptr && the_node->right == nullptr){
+
+                    if((the_node->parent)->right == the_node){
+                        ((the_node)->parent)->right = the_node->left;
+                        (the_node->right)->parent = the_node->parent;
+                        delete the_node;
+                        nodes--;
+
+                    }else{
+
+                        ((the_node)->parent)->left = the_node->left;
+                        (the_node->right)->parent = the_node->parent;
+                        delete the_node;
+                        nodes--;  
+
+                    }
+                }else{
+                    bs_node * valnode = maximum(the_node->left);
+                    int d = valnode->data;
+                    delete_node(d);
+                    the_node->data = d;
                 }
+            }
+        }
+
+        void pre_order_traversal(bs_node * node, vector<int> * vec){
+            if(node != nullptr){
+                (*vec).push_back(node->data);
+                pre_order_traversal(node->left, vec);
+                pre_order_traversal(node->right, vec);
+            }
+
+        }
+
+        void in_order_traversal(bs_node * node, vector<int> * vec){
+            if(node != nullptr){
+                in_order_traversal(node->left, vec);
+                (*vec).push_back(node->data);
+                in_order_traversal(node->right, vec);
+            }
+        }
+
+        void post_order_traversal(bs_node * node, vector<int> * vec){
+            if(node != nullptr){
+                post_order_traversal(node->left, vec);
+                post_order_traversal(node->right, vec);
+                (*vec).push_back(node->data);
+            }
+        }
+
+        void level_order_traversal(bs_node * node, vector<int> * vec, queue<bs_node *> * q){
+            if(node != nullptr){
+                (*vec).push_back(node->data);
+                (*q).push(node->left);
+                (*q).push(node->right);
+                bs_node * newnode = (*q).front();
+                (*q).pop();
+                level_order_traversal(newnode, vec, q);
             }
         }
 
